00b3942c40c4d65ae819afcdd7da09ada68252b7
Björn Bohman <bjorn.bohman@internetz.se>
2018-05-24 20:10:58 +0200

    Loop over multiple zones. Send both A and CNAME to stdout

--- client/client.go
+++ client/client.go
@@ -14,41 +14,39 @@ func main() {
 		os.Exit(1)
 	}
 	hostToGet := os.Args[1]
-	aRR := make(map[string]uint16)
-	cnameRR := make(map[string]uint16)
+	dnsRR := map[string]uint16{}
 	//	t := new(dns.Transfer)
 	//	m := new(dns.Msg)
-	t := &dns.Transfer{}
-	m := &dns.Msg{}
-	m.SetAxfr("***REMOVED***")
-	c, err := t.In(m, "***REMOVED***:53")
-	if err != nil {
-		fmt.Println("Got error: ", err)
-		os.Exit(1)
-	}
+	for _, z := range []string{"***REMOVED***", "***REMOVED***", "db.***REMOVED***", "***REMOVED***"} {
+		t := &dns.Transfer{}
+		m := &dns.Msg{}
+		m.SetAxfr(z)
+		c, err := t.In(m, "***REMOVED***:53")
+		if err != nil {
+			fmt.Println("Got error: ", err)
+			os.Exit(1)
+		}
 
-	for envelope := range c { // Range read from channel c
-		for _, rr := range envelope.RR { // Iterate over all Resource Records
-			name := strings.TrimRight(rr.Header().Name, ".")
-			rrtype := rr.Header().Rrtype
-			//ttl := rr.Header().Ttl
+		for envelope := range c { // Range read from channel c
+			for _, rr := range envelope.RR { // Iterate over all Resource Records
+				name := strings.TrimRight(rr.Header().Name, ".")
+				rrtype := rr.Header().Rrtype
+				//ttl := rr.Header().Ttl
 
-			if strings.Contains(name, hostToGet) {
-				if rrtype == dns.TypeA {
-					aRR[name] = rrtype
-				}
-				if rrtype == dns.TypeCNAME {
-					cnameRR[name] = rrtype
+				if strings.Contains(name, hostToGet) {
+					if rrtype == dns.TypeA || rrtype == dns.TypeCNAME {
+						dnsRR[name] = rrtype
+					}
 				}
 			}
 		}
 	}
-
 	keys := []string{}
-	for k := range aRR {
+	for k := range dnsRR {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
+
 	for _, hostname := range keys {
 		fmt.Println(hostname)
 	}
